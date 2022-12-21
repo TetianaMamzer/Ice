ae8fc9cb3036e6ae40eec1fe439809ee3d2ed55f
TetianaMamzer <tetianamamzer@gmail.com>
2022-12-21 12:00:28 +0200

    +

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,10 +2,13 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    closeList: document.querySelector('.modal-menu__group'),
     menu: document.querySelector('[data-menu]'),
   };
+  console.log(refs);
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.closeList.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
   }
